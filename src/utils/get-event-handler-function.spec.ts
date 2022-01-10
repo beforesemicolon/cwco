59be491d0fcafae2c03232dd2528887141c92c0f
Elson Correia <design@elsoncorreia.com>
2022-01-10 07:47:38 -0500

    better support for inline event handling

--- src/utils/get-event-handler-function.spec.ts
+++ src/utils/get-event-handler-function.spec.ts
@@ -26,12 +26,85 @@ describe('getEventHandlerFunction', () => {
 		expect(res?.toString()).toEqual('(event) => func.call(component, event, ...values)');
 	});
 
-	it('should return null if invalid value', () => {
-		const attr = {name: 'onclick', value: 'null'};
+	describe('should get listener', () => {
+		class MyButton extends WebComponent {
+			onClick() {}
 
-		const res = getEventHandlerFunction(app, {}, attr as Attr);
+			sampler = [];
 
-		expect(res).toEqual(null);
-		expect(errorSpy).toHaveBeenCalledWith(new Error('EventApp: Invalid event handler for "onclick" >>> "null".'));
+			clicked = () => {}
+		}
+		MyButton.register();
+		const btn = new MyButton();
+
+		it('if function exists', () => {
+			btn.onClick = jest.fn();
+			btn.clicked = jest.fn();
+
+			let handler = getEventHandlerFunction(btn as any, {}, {name: 'click', value: 'clicked("sample", 300)'} as Attr) as any;
+
+			expect(handler.toString()).toEqual('(event) => func.call(component, event, ...values)')
+
+			handler({type: 'click'} as any);
+
+			expect(btn.clicked).toHaveBeenCalledWith("sample", 300);
+
+			// @ts-ignore
+			btn.onClick.mockClear();
+
+			handler = getEventHandlerFunction(btn as any, {}, {name: 'click', value: 'this.onClick($event, 12, [23, 45])'} as Attr);
+
+			handler({type: 'click'} as any);
+
+			expect(btn.onClick).toHaveBeenCalledWith({"type": "click"}, 12, [23, 45]);
+
+			handler = getEventHandlerFunction(btn as any, {$item: [23, 45]}, {name: 'click', value: 'onClick($item)'} as Attr);
+
+			handler({type: 'click'} as any);
+
+			expect(btn.onClick).toHaveBeenCalledWith([23, 45]);
+
+			jest.resetAllMocks();
+		});
+
+		it('with executables', () => {
+			let handler = getEventHandlerFunction(btn as any, {}, {name: 'click', value: '{this.sampler = [2, 4, 6]}'} as Attr) as any;
+
+			expect(handler.toString()).toEqual('(event) => func.call(component, event, ...values)');
+
+			handler({type: 'click'} as any);
+
+			expect(btn.sampler).toEqual([
+				2,
+				4,
+				6
+			]);
+
+			handler = getEventHandlerFunction(btn as any, {$item: {x: 10}}, {name: 'click', value: '{this.sampler = $item}'} as Attr) as any;
+
+			handler({type: 'click'} as any);
+
+			expect(btn.sampler).toEqual({x: 10});
+		});
+
+		it('without executables', () => {
+			let handler = getEventHandlerFunction(btn as any, {}, {name: 'click', value: 'this.sampler = [2, 4, 6]'} as Attr) as any;
+
+			expect(handler.toString()).toEqual('(event) => func.call(component, event, ...values)');
+
+			handler({type: 'click'} as any);
+
+			expect(btn.sampler).toEqual([
+				2,
+				4,
+				6
+			]);
+
+			handler = getEventHandlerFunction(btn as any, {$item: {x: 10}}, {name: 'click', value: '{this.sampler = $item}'} as Attr) as any;
+
+			handler({type: 'click'} as any);
+
+			expect(btn.sampler).toEqual({x: 10});
+		});
 	});
 });
