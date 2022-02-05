2594e14453e65da20e9f96e6340ce5ea358b14d3
Elson Correia <design@elsoncorreia.com>
2022-02-05 04:38:54 -0500

    remove js in src

--- src/enums/track-type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.TrackType = void 0;
-var TrackType;
-(function (TrackType) {
-    TrackType[TrackType["attribute"] = 0] = "attribute";
-    TrackType[TrackType["property"] = 1] = "property";
-    TrackType[TrackType["directive"] = 2] = "directive";
-})(TrackType = exports.TrackType || (exports.TrackType = {}));
--- src/utils/is-primitive.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isPrimitive = void 0;
-var isPrimitive = function (val) {
-    return /number|string|bigint|boolean|symbol/.test(typeof val);
-};
-exports.isPrimitive = isPrimitive;
